9fbcbf570879b8f8030ae880dec994a0fecfcff0
frank <frank.danil704@gmail.com>
2024-06-19 12:48:00 +0300

    add test report

--- src/database.py
+++ src/database.py
@@ -1,8 +1,9 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 from config import settings
 
-async_engine = create_async_engine(settings.DB_URL)
+async_engine = create_async_engine(settings.DB_URL, poolclass=NullPool)
 async_session_factory = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
 
 
--- src/repositories/base.py
+++ src/repositories/base.py
@@ -24,29 +24,29 @@ class DatabaseRepository(AbstractRepository):
     model = None
     LIMIT = 100
 
-    def __init__(self):
-        self.session: AsyncSession = async_session_factory()
-
     async def get_last_by_count(self, count: int):
         """Метод для получения последних n записей"""
-        limit = self.LIMIT if count > self.LIMIT else abs(count)
-        query = select(self.model.trading_date).distinct().order_by(self.model.trading_date.desc()).limit(limit)
-        result = await self.session.execute(query)
-        return result.scalars().all()
+        async with async_session_factory() as session:
+            limit = self.LIMIT if count > self.LIMIT else abs(count)
+            query = select(self.model.trading_date).distinct().order_by(self.model.trading_date.desc()).limit(limit)
+            result = await session.execute(query)
+            return result.scalars().all()
 
     async def get_last(self, data: dict[str, str]):
         """Метод для получения записей по фильтрам"""
-        filters = {k: v for k, v in data.items() if v}
-        query = select(self.model).filter_by(**filters).limit(self.LIMIT)
-        result = await self.session.execute(query)
-        return result.scalars().all()
+        async with async_session_factory() as session:
+            filters = {k: v for k, v in data.items() if v}
+            query = select(self.model).filter_by(**filters).limit(self.LIMIT)
+            result = await session.execute(query)
+            return result.scalars().all()
 
     async def get_list_by_period(self, data: dict[str, str]):
         """Метод для получения записей за определенный период"""
-        filters = {k: v for k, v in data.items() if v}
-        query = select(self.model).where(and_(
-            self.model.trading_date >= filters.pop('start_date'),
-            self.model.trading_date < filters.pop('end_date'))
-        ).filter_by(**filters).limit(self.LIMIT)
-        result = await self.session.execute(query)
-        return result.scalars().all()
+        async with async_session_factory() as session:
+            filters = {k: v for k, v in data.items() if v}
+            query = select(self.model).where(and_(
+                self.model.trading_date >= filters.pop('start_date'),
+                self.model.trading_date < filters.pop('end_date'))
+            ).filter_by(**filters).limit(self.LIMIT)
+            result = await session.execute(query)
+            return result.scalars().all()
